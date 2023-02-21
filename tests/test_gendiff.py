9556de9d111b5755320dcc6ce23aa543a5add9bc
Andrey Bogatyrev <t2way@yandex.ru>
2023-02-21 18:28:01 +0300

    add test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,14 +1,22 @@
-'''from gendiff.gendiff import generate_diff
+from gendiff.gendiff import generate_diff
 
 def test_generate_diff():
     assert generate_diff({
     "host": "hexlet.io",
     "timeout": 50,
     "proxy": "123.234.53.22",
-    "follow": false
+    "follow": False
   },
   {
     "timeout": 20,
-    "verbose": true,
+    "verbose": True,
     "host": "hexlet.io"
-  }) == '''
+  }) == (
+        '{\n  - follow: False'
+        '\n    host: hexlet.io'
+        '\n  - proxy: 123.234.53.22'
+        '\n  - timeout: 50'
+        '\n  + timeout: 20'
+        '\n  + verbose: True'
+        '\n}'
+  )
