300108a64de4f6e09fe404a98cc9de8335f37bf3
Andrey Bogatyrev <t2way@yandex.ru>
2023-02-23 02:17:43 +0300

    add tests/fixtures with file1.json, file2.json, result.txt correct test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,4 +1,12 @@
 from gendiff.gendiff import generate_diff
+import json
+
+'''with open('tests/fixtures/file1.json') as f:
+    data1 = json.load(f)
+with open('tests/fixtures/file2.json') as f:
+    data2 = json.load(f)
+with open('tests/fixtures/result.txt') as f:
+    right_result = f.read()'''
 
 data1 = {
     "host": "hexlet.io",
@@ -20,6 +28,7 @@ right_result = (
         '\n  + verbose: True'
         '\n}'
   )
+
 
 def test_generate_diff():
     assert generate_diff(data1, data2) == right_result
