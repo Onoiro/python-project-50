81c1426a51db13c54cc1d59fa8d6fb07cc3dc338
Andrey Bogatyrev <t2way@yandex.ru>
2023-02-22 16:41:03 +0300

    update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,17 +1,17 @@
 from gendiff.gendiff import generate_diff
 
-def test_generate_diff():
-    assert generate_diff({
+data1 = {
     "host": "hexlet.io",
     "timeout": 50,
     "proxy": "123.234.53.22",
     "follow": False
-  },
-  {
+  }
+data2 = {
     "timeout": 20,
     "verbose": True,
     "host": "hexlet.io"
-  }) == (
+  }
+right_result = (
         '{\n  - follow: False'
         '\n    host: hexlet.io'
         '\n  - proxy: 123.234.53.22'
@@ -20,3 +20,6 @@ def test_generate_diff():
         '\n  + verbose: True'
         '\n}'
   )
+
+def test_generate_diff():
+    assert generate_diff(data1, data2) == right_result
