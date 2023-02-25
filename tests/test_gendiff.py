c0e1b3af3ce41e6fcbc39a958226f1f0f6a75eee
Andrey Bogatyrev <t2way@yandex.ru>
2023-02-25 15:02:00 +0300

    install pyyaml and update test_gendiff.py fix poetry.lock and pyproject.toml

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,34 +1,20 @@
 from gendiff.gendiff import generate_diff
 import json
+import yaml
+from yaml.loader import SafeLoader
 
 with open('tests/fixtures/file1.json') as f:
     data1 = json.load(f)
 with open('tests/fixtures/file2.json') as f:
     data2 = json.load(f)
+with open('tests/fixtures/filepath1.yml') as f:
+    data3 = yaml.load(f, Loader=SafeLoader)
+with open('tests/fixtures/filepath2.yml') as f:
+    data4 = yaml.load(f, Loader=SafeLoader)
 with open('tests/fixtures/result.txt') as f:
     right_result = f.read()
-
-'''data1 = {
-    "host": "hexlet.io",
-    "timeout": 50,
-    "proxy": "123.234.53.22",
-    "follow": False
-  }
-data2 = {
-    "timeout": 20,
-    "verbose": True,
-    "host": "hexlet.io"
-  }
-right_result = (
-        '{\n  - follow: False'
-        '\n    host: hexlet.io'
-        '\n  - proxy: 123.234.53.22'
-        '\n  - timeout: 50'
-        '\n  + timeout: 20'
-        '\n  + verbose: True'
-        '\n}'
-  )'''
 
 
 def test_generate_diff():
     assert generate_diff(data1, data2) == right_result
+    assert generate_diff(data3, data4) == right_result
