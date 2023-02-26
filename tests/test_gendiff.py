9c9acbab7eb9cca82e3205b708f446bcdff1d185
Andrey Bogatyrev <t2way@yandex.ru>
2023-02-26 17:28:42 +0300

    remove .jml files and add .yml files to fixtures/

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -13,8 +13,20 @@ with open('tests/fixtures/filepath2.yml') as f:
     data4 = yaml.load(f, Loader=SafeLoader)
 with open('tests/fixtures/result.txt') as f:
     right_result = f.read()
+with open('tests/fixtures/file3.json') as f:
+    data5 = json.load(f)
+with open('tests/fixtures/file4.json') as f:
+    data6 = json.load(f)
+with open('tests/fixtures/filepath3.yml') as f:
+    data7 = yaml.load(f, Loader=SafeLoader)
+with open('tests/fixtures/filepath4.yml') as f:
+    data8 = yaml.load(f, Loader=SafeLoader)
+with open('tests/fixtures/expected_result_2.txt') as f:
+    right_result = f.read()
 
 
 def test_generate_diff():
     assert generate_diff(data1, data2) == right_result
     assert generate_diff(data3, data4) == right_result
+    assert generate_diff(data5, data6) == right_result
+    assert generate_diff(data7, data8) == right_result
