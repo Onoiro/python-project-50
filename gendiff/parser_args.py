bd224f8ceb8baa6453f268dcc2e11333bd3c4cbf
Andrey Bogatyrev <t2way@yandex.ru>
2023-03-13 17:01:16 +0300

    update parser_args.py: change get_parsed_data() to get_parsed_args() so modify scripts/gendiff.py

--- gendiff/parser_args.py
+++ gendiff/parser_args.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def get_parsed_data():
+def get_parsed_args():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 from gendiff.gendiff import generate_diff
-from gendiff.parser_args import get_parsed_data
+from gendiff.parser_args import get_parsed_args
 
 
 def main():
-    filepath1, filepath2, format_type = get_parsed_data()
+    filepath1, filepath2, format_type = get_parsed_args()
     print(generate_diff(filepath1, filepath2, format_type))
 
 
