cd94a36dacbee24e16409cd64382a3c8a683f6a3
Andrey Bogatyrev <t2way@yandex.ru>
2023-03-14 16:41:33 +0300

    update test_gendiff.py: rename some files in fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -23,13 +23,13 @@ with open('tests/fixtures/filepath3.yml') as f:
     yaml_data_3 = yaml.load(f, Loader=SafeLoader)
 with open('tests/fixtures/filepath4.yml') as f:
     yaml_data_4 = yaml.load(f, Loader=SafeLoader)
-with open('tests/fixtures/expected_result_1.txt') as f:
-    expected_result_1 = f.read()
-with open('tests/fixtures/expected_result_2.txt') as f:
-    expected_result_2 = f.read()
+with open('tests/fixtures/expected_stylish_1.txt') as f:
+    expected_stylish_result_1 = f.read()
+with open('tests/fixtures/expected_stylish_2.txt') as f:
+    expected_stylish_result_2 = f.read()
 with open('tests/fixtures/expected_plain_1.txt') as f:
     expected_plain_result_1 = f.read()
-with open('tests/fixtures/expected_plain.txt') as f:
+with open('tests/fixtures/expected_plain_2.txt') as f:
     expected_plain_result_2 = f.read()
 diff_list_json_1 = get_diffs(json_data_1, json_data_2)
 diff_list_yaml_1 = get_diffs(yaml_data_1, yaml_data_2)
@@ -38,10 +38,10 @@ diff_list_yaml_2 = get_diffs(yaml_data_3, yaml_data_4)
 
 
 @pytest.mark.parametrize("test_input, expected",
-                        [(diff_list_json_1, expected_result_1),
-                         (diff_list_yaml_1, expected_result_1),
-                         (diff_list_json_2, expected_result_2),
-                         (diff_list_yaml_2, expected_result_2),
+                        [(diff_list_json_1, expected_stylish_result_1),
+                         (diff_list_yaml_1, expected_stylish_result_1),
+                         (diff_list_json_2, expected_stylish_result_2),
+                         (diff_list_yaml_2, expected_stylish_result_2),
                         ])
 def test_get_stylish(test_input, expected):
     assert get_stylish(test_input) == expected
