ac826c8909c37365df5a6275eb9e28945b4e0bad
Andrey Bogatyrev <t2way@yandex.ru>
2023-03-18 18:52:03 +0300

    correct file_parser.py: extention instead extencion

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -3,8 +3,8 @@ import yaml
 from yaml.loader import SafeLoader
 
 
-def get_dict(obj, extencion):
-    if extencion == 'json':
+def get_dict(obj, extention):
+    if extention == 'json':
         result_dict = json.loads(obj)
     else:
         result_dict = yaml.load(obj, Loader=SafeLoader)
