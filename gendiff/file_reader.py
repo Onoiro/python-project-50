d729490a6f3728bf4a062a2e7c61b47539f1ed1f
Andrey Bogatyrev <t2way@yandex.ru>
2023-03-21 17:11:23 +0300

    update file_reader.py: add get_extention() like separate func

--- gendiff/file_reader.py
+++ gendiff/file_reader.py
@@ -1,8 +1,12 @@
 from gendiff.file_parser import get_object
 
 
-def get_data(filepath):
+def get_extention(filepath):
     extention = filepath.split('.')[-1]
+    return extention
+
+
+def get_data(filepath):
     with open(filepath) as f:
-        obj = f.read()
-    return get_object(obj, extention)
+        data = f.read()
+    return get_object(data, get_extention(filepath))
